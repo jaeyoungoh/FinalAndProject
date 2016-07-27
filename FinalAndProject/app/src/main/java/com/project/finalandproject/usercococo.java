07dd5fffb5a6f7bf66a0b35764f9c713538d26c9
jhpark <food9576@gmail.com>
2016-07-27 14:56:51 +0900

    test3

--- FinalAndProject/app/src/main/java/com/project/finalandproject/usercococo.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/usercococo.java
@@ -1,7 +1,15 @@
 package com.project.finalandproject;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2016-07-27.
  */
-public class usercococo {
+public class usercococo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test2layout);
+    }
 }
