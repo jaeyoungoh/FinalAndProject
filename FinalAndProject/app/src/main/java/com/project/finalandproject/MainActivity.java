c9550214aa588f550e27cded223d0c381729a1e1
jhpark <food9576@gmail.com>
2016-08-02 09:11:38 +0900

    캘린터 테스트

--- FinalAndProject/app/src/main/java/com/project/finalandproject/MainActivity.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test2layout);
+        setContentView(R.layout.test_calendar);
     }
 }
--- FinalAndProject/app/src/main/java/com/project/finalandproject/usercococo.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/usercococo.java
@@ -2,14 +2,34 @@ package com.project.finalandproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2016-07-27.
  */
 public class usercococo extends Activity {
+
+    CalendarView calendar;
+    TextView text11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test2layout);
+        setContentView(R.layout.test_calendar);
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        text11 = (TextView)findViewById(R.id.text11);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayy) {
+                month++;
+                text11.setText(year+" 년 " + month +" 월 " + dayy+" 일 ");
+                Toast.makeText(getApplicationContext(), "Year : " + year + "\n" +
+                        "Month : " + month +  "\n" +
+                        "Day : " + dayy, Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
