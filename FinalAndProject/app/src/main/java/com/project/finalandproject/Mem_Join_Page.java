7e13d1a1d5d1f377cda74bd97343bfa4907aac66
jhpark <food9576@gmail.com>
2016-08-02 10:12:05 +0900

    취소버튼 기능 추가.

--- FinalAndProject/app/src/main/java/com/project/finalandproject/Mem_Join_Page.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/Mem_Join_Page.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Administrator on 2016-08-02.
@@ -12,15 +13,29 @@ import android.widget.Button;
 public class Mem_Join_Page extends Activity {
 
     Intent intent;
-    Button goback;
+    Button goback,cancle;
+    int texts[] = {R.id.join_name_txt, R.id.join_id_txt, R.id.join_pass_txt, R.id.join_email_txt};
+    EditText edtTxt[] = new EditText[texts.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.memjoin_page);
         goback = (Button) findViewById(R.id.goback);
+        cancle = (Button) findViewById(R.id.cancle);
+        for (int i=0; i<texts.length; i++){
+            edtTxt[i] = (EditText)findViewById(texts[i]);
+        }
 
 
+    }
+
+    public void Cancle(View v){
+        if(v.getId()==cancle.getId()){
+            for (int i=0; i<texts.length; i++){
+                edtTxt[i].setText("");
+            }
+        }
     }
 
 
