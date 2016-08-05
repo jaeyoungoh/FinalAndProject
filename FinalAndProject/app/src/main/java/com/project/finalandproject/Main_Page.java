3aee66e9d86dcd68d49370885ba4d17d7fc73db1
오재영 <wodud.oh@gmail.com>
2016-08-05 13:32:34 +0900

    진혁이 로그인 완료

--- FinalAndProject/app/src/main/java/com/project/finalandproject/Main_Page.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/Main_Page.java
@@ -3,8 +3,13 @@ package com.project.finalandproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.project.finalandproject.dto.MemberDTO;
 
 /**
  * Created by Administrator on 2016-08-02.
@@ -14,21 +19,39 @@ public class Main_Page extends Activity {
     Intent intent;
     Button Joinbt;
 
+    EditText member_id;
+    EditText member_pwd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
         Joinbt = (Button)findViewById(R.id.joinbt);
+        member_id = (EditText)findViewById(R.id.num1);
+        member_pwd = (EditText)findViewById(R.id.num2);
 
 
     }
 
     public void Joinbt(View v){
-        if(v.getId()==Joinbt.getId()){
-            intent = new Intent(getApplication(), Mem_Join_Page.class);
-            startActivity(intent);
-        }
+        intent = new Intent(getApplication(), Mem_Join_Page.class);
+        startActivity(intent);
+    }
+
+    public void Login(View v){
+
+        Log.d("로그인","버튼누름");
+        MemberDTO m = new MemberDTO();
+        m.setId(member_id.getText().toString());
+        m.setPwd(member_pwd.getText().toString());
+
+        if(Info.connServer("login", m)){
+            Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+
+
     }
 
 
