8c0159ab560304563b53cbfab4055f74df0c15b5
오재영 <wodud.oh@gmail.com>
2016-08-05 13:56:41 +0900

    java 파일 이동에 관한 건

---
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/member/Mem_Join_Success_page.java
@@ -0,0 +1,17 @@
+package com.project.finalandproject.member;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.project.finalandproject.R;
+
+/**
+ * Created by Administrator on 2016-08-04.
+ */
+public class Mem_Join_Success_page extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mem_join_success_page);
+    }
+}
---
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/member/Mem_Modify_Page.java
@@ -0,0 +1,19 @@
+package com.project.finalandproject.member;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.project.finalandproject.R;
+
+/**
+ * Created by Administrator on 2016-08-02.
+ */
+public class Mem_Modify_Page extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mem_modify_page);
+
+    }
+}
