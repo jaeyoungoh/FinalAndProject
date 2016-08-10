4ed66ca4c81b3eb817a9803e8a2d2be2f961b3de
김희윤 <qhfkdlgl@naver.com>
2016-08-10 17:34:44 +0900

    라운드버튼

--- FinalAndProject/app/src/main/java/com/project/finalandproject/member/Mem_Join_Page.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/member/Mem_Join_Page.java
@@ -1,13 +1,18 @@
 package com.project.finalandproject.member;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.project.finalandproject.R;
@@ -22,6 +27,7 @@ import org.apache.http.message.BasicNameValuePair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 
 public class Mem_Join_Page extends Activity {
@@ -36,7 +42,22 @@ public class Mem_Join_Page extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mem_join_page);
+        setContentView(R.layout.top_menu_layout);
+        LinearLayout container=(LinearLayout) findViewById(R.id.container);
+        TextView title=(TextView) findViewById(R.id.title);
+        TextView next=(TextView) findViewById(R.id.next);
+        next.setText("가입");
+        title.setText("회원가입");
+        ImageButton back=(ImageButton) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        LayoutInflater inflater=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.mem_join_page, container,true);
         cancle = (Button) findViewById(R.id.cancle);
         joinsubmit = (Button) findViewById(R.id.joinsubmit);
         for (int i = 0; i < texts.length; i++) {
