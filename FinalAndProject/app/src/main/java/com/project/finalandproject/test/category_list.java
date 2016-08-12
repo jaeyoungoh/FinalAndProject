f6aa8fc5e942dce3a14cf91fcea0eff24d7e656d
김희윤 <qhfkdlgl@naver.com>
2016-08-12 09:59:38 +0900

    카테고리

--- FinalAndProject/app/src/main/java/com/project/finalandproject/test/category_list.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/test/category_list.java
@@ -3,11 +3,15 @@ package com.project.finalandproject.test;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.finalandproject.R;
 
@@ -15,7 +19,8 @@ import com.project.finalandproject.R;
  * Created by Administrator on 2016-08-10.
  */
 public class category_list extends Activity{
-
+    int count=0;
+    String categorytxt="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +41,35 @@ public class category_list extends Activity{
 
         LayoutInflater inflater=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.category, container,true);
+        final GridLayout category=(GridLayout) findViewById(R.id.category);
+        for(int i=0;i<category.getChildCount();i++){
+            category.getChildAt(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        select(view);
+                }
+            });
+        }
     }
 
+    private void select(View view){
 
+            if(categorytxt.indexOf((((Button) view).getText().toString() + ","))!=-1) {
+                count--;
+                categorytxt = categorytxt.replace((((Button) view).getText().toString() + ","), "");
+                view.setSelected(false);
+            }else{
+            if(count<3) {
+                count++;
+                categorytxt += ((Button) view).getText().toString() + ",";
+                view.setSelected(true);
+            }else{
+                Toast.makeText(getApplicationContext(),"카테고리는 3개만 선택해 주세요.",Toast.LENGTH_SHORT).show();
+            }
+            }
+        Log.i("category",categorytxt+","+count);
+
+
+
+    }
 }
