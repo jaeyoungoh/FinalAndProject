c2aae25a5d443cc93d26f360c9a9d6f29c9cecab
오재영 <wodud.oh@gmail.com>
2016-08-12 15:12:46 +0900

    멤버 관련한 Request URL 주소 넘겨주는거 초기화 적용 완료.

--- FinalAndProject/app/src/main/java/com/project/finalandproject/conn/MemConn.java
+++ FinalAndProject/app/src/main/java/com/project/finalandproject/conn/MemConn.java
@@ -28,24 +28,24 @@ import java.util.List;
  * Created by Administrator on 2016-08-11.
  */
 public class MemConn {
-    static private String requestURL="http://192.168.14.31:8805/finalproject/";
+    static private StringBuffer requestURL= new StringBuffer("http://192.168.14.31:8805/finalproject/");
+
     static private List<NameValuePair> setParamList(String type, Object obj){
         List<NameValuePair> paramList = new ArrayList<>();
 
         if(type.equals("login")){
-            requestURL += "login.do";
+            requestURL.append("login.do");
             paramList.add(new BasicNameValuePair("id", ((MemberDTO) obj).getId()));
             paramList.add(new BasicNameValuePair("pwd", ((MemberDTO) obj).getPwd()));
-
         } else if(type.equals("join")){
-            requestURL += "join.do";
+            requestURL.append("join.do");
             paramList.add(new BasicNameValuePair("name", ((MemberDTO) obj).getName()));
             paramList.add(new BasicNameValuePair("id", ((MemberDTO) obj).getId()));
             paramList.add(new BasicNameValuePair("pwd", ((MemberDTO) obj).getPwd()));
             paramList.add(new BasicNameValuePair("email", ((MemberDTO) obj).getEmail()));
 
         } else if(type.equals("makeProfile")){
-            requestURL += "makeprofile.do";
+            requestURL.append("makeprofile.do");
             paramList.add(new BasicNameValuePair("id", MemInfo.USER_ID));
             paramList.add(new BasicNameValuePair("interest", ((MemberDTO) obj).getInterest()));
             paramList.add(new BasicNameValuePair("location", ((MemberDTO) obj).getLocation()));
@@ -54,10 +54,11 @@ public class MemConn {
             paramList.add(new BasicNameValuePair("sex", ((MemberDTO) obj).getSex()));
             paramList.add(new BasicNameValuePair("phone", ((MemberDTO) obj).getPhone()));
         } else if (type.equals("info")){
-            requestURL = "makeprofile.do";
-            paramList.add(new BasicNameValuePair("id", MemInfo.USER_ID));
+            requestURL.append("info.do");
+//            paramList.add(new BasicNameValuePair("id", MemInfo.USER_ID));
+            Log.i("result","요청할 URL 주소 : "+requestURL.toString());
+            paramList.add(new BasicNameValuePair("id", "1234"));
         }
-
         return paramList;
     }
 
@@ -71,7 +72,9 @@ public class MemConn {
 
         try {
             HttpClient client = new DefaultHttpClient();
-            HttpPost post = new HttpPost(requestURL);
+            HttpPost post = new HttpPost(requestURL.toString());
+            requestURL.setLength(39);// 주소 초기화
+            Log.i("result","초기화 된 URL 주소 : "+requestURL.toString());
             post.setEntity(new UrlEncodedFormEntity(paramList, "UTF-8"));
             HttpResponse response = client.execute(post);
             HttpEntity entity = response.getEntity();
@@ -83,12 +86,12 @@ public class MemConn {
             while((line = rd.readLine()) != null) {
                 sb.append(line);
             }
+            Log.i("result","JSON 넘어온 값"+sb.toString());
 
             //로그인과 회원가입시 JSONOBJECT 파싱
             if(type.equals("join")||type.equals("login")||type.equals("info")){
                 JSONParser parser =  new JSONParser();
                 JSONObj = (JSONObject)parser.parse(sb.toString());
-                Log.i("============>>>>>>>","JSON 넘어온 값"+sb.toString());
             }else if(type.equals("update")){
 
             }
